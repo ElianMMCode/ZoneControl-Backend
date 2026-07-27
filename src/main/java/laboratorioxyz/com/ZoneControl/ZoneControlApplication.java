437ef9975f2f4c58358ab428f6697a9162291bc0
ElianMMCode <emelo.dev@pm.me>
2026-07-26 19:46:51 -0500

    Inicial commit

---
+++ src/main/java/laboratorioxyz/com/ZoneControl/ZoneControlApplication.java
@@ -0,0 +1,13 @@
+package laboratorioxyz.com.ZoneControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoneControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoneControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/laboratorioxyz/com/ZoneControl/ZoneControlApplicationTests.java
@@ -0,0 +1,13 @@
+package laboratorioxyz.com.ZoneControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoneControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
